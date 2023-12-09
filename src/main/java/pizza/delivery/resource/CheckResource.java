9e53232aba06e9972aad1223c0009fce7166325f
Anastasiaza333 <anastasiaza05@gmail.com>
2023-12-09 20:44:13 +0200

    CheckResource

--- src/main/java/pizza/delivery/resource/CheckResource.java
+++ src/main/java/pizza/delivery/resource/CheckResource.java
@@ -1,8 +1,53 @@
 package pizza.delivery.resource;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pizza.delivery.dto.CheckDTO;
+import pizza.delivery.service.CheckService;
+
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/check")
-@RequiredArgsConstructor
+@RequestMapping("/api/checks")
 public class CheckResource {
+
     private final CheckService checkService;
+
+    @Autowired
+    public CheckResource(CheckService checkService) {
+        this.checkService = checkService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CheckDTO> getCheckById(@PathVariable Long id) {
+        CheckDTO checkDTO = checkService.getCheckById(id);
+        return ResponseEntity.ok(checkDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CheckDTO>> getAllChecks() {
+        List<CheckDTO> checks = checkService.getAllChecks();
+        return ResponseEntity.ok(checks);
+    }
+
+    @PostMapping
+    public ResponseEntity<CheckDTO> createCheck(@RequestBody @Valid CheckDTO checkDTO) {
+        CheckDTO createdCheck = checkService.createCheck(checkDTO);
+        return new ResponseEntity<>(createdCheck, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CheckDTO> updateCheck(@PathVariable Long id, @RequestBody @Valid CheckDTO checkDTO) {
+        CheckDTO updatedCheck = checkService.updateCheck(id, checkDTO);
+        return ResponseEntity.ok(updatedCheck);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCheck(@PathVariable Long id) {
+        checkService.deleteCheck(id);
+        return ResponseEntity.noContent().build();
+    }
 }
