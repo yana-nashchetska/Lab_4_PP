dd9f7eb84d3e94bcd68c7351e9901569bd798579
lesiukoksana <114728948+lesiukoksana@users.noreply.github.com>
2023-12-09 16:29:15 +0200

    first CustomerRepository

--- src/main/java/pizza/delivery/repository/CustomerRepository.java
+++ src/main/java/pizza/delivery/repository/CustomerRepository.java
@@ -2,6 +2,15 @@ package pizza.delivery.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import pizza.delivery.entity.Customer;
+import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findOneByNameAndSurname(String firstName, String lastName);
+    List<Customer> findAllByIsActiveTrue();
 }
