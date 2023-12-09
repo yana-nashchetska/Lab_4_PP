49232859379dc3b3adbe40ca908cffc25f6fa243
yana-nashchetska <115204812+yana-nashchetska@users.noreply.github.com>
2023-12-09 22:30:11 +0200

    Check resource (#12)
    * CheckRepository
    
    * CheckStreamRepository
    
    * CheckResource
    
    ---------
    
    Co-authored-by: Anastasiaza333 <anastasiaza05@gmail.com>

--- src/main/java/pizza/delivery/repository/stream/CheckStreamRepository.java
+++ src/main/java/pizza/delivery/repository/stream/CheckStreamRepository.java
@@ -1,6 +1,43 @@
 package pizza.delivery.repository.stream;
 
-public class CheckStreamRepository {
+import pizza.delivery.entity.Check;
 
-    //TODO: implement this class
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class CheckStreamRepository {
+    private List<Check> checkList = new ArrayList<>();
+
+    private Long lastCheckId = 0L;
+
+    private Long generateId() {
+        ++lastCheckId;
+        return lastCheckId;
+    }
+
+    public Check save(final Check check) {
+        check.setId(generateId());
+        checkList.add(check);
+        return check;
+    }
+
+    public List<Check> findAll() {
+        return checkList;
+    }
+
+    // TODO: Check update
+
+    public Optional<Check> findById(final Long id) {
+        return checkList.stream()
+                .filter(e -> e.getId().equals(id))
+                .findFirst();
+    }
+
+    public void deleteById(final Long id) {
+        checkList = checkList.stream()
+                .filter(e -> !e.getId().equals(id))
+                .collect(Collectors.toList());
+    }
 }
